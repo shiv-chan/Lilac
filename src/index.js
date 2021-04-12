abc5940d51b8b1ef4c086dbb03d5158d45e1cbdc
shiv-chan <kshibuya31@gmail.com>
2021-04-12 16:36:21 +0900

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import _ from 'lodash';
+const storedNums = [];
 
 function addRandomNum() {
   const mainEl = document.getElementById('main');
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
+  let p = document.createElement('p');
+  let randNum = _.random(0, 10);
+  p.innerHTML = "Random Number: " + randNum.toString();
   document.body.appendChild(p)
+  storedNums.push(randNum);
+  if(storedNums.length > 5){
+    storedNums.shift();
+  } 
+  console.log(storedNums);
+}
+
+function recordNumber() {
+  const mainEl = document.getElementById('main');
+  let p = document.createElement('p');
+  p.textContent = `Sum of the Last 5 Random Numbers: ${_.sum(storedNums)}`;
+  document.body.appendChild(p);
 }
 
 {
   const button1 = document.getElementById('button1')
   button1.addEventListener("click", addRandomNum);
+  const button2 = document.getElementById('button2');
+  button2.addEventListener("click", recordNumber);
   console.log("ready");
 }
