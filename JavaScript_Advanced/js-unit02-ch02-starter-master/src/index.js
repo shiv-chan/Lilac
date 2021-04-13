5ddf0166efc815133b8b9e73c434bd27f0088565
shiv-chan <kshibuya31@gmail.com>
2021-04-13 19:03:17 +0900

    Update files.
    Update functions and Add style.

--- JavaScript_Advanced/js-unit02-ch02-starter-master/src/index.js
+++ JavaScript_Advanced/js-unit02-ch02-starter-master/src/index.js
@@ -20,12 +20,42 @@ function handleClick(e) {
   /* 
     getDataを呼び出して、mainEl.innerHTMLを利用して、結果を出力します。
   */
+  const table = document.createElement('table');
+  const p = document.createElement('p');
+
+  getData()
+    .then(data => {
+      mainEl.innerHTML = '';
+      const tableContents = [];
+      for(const key in data){
+        if(typeof data[key] !== 'object'){
+          tableContents.push(`<tr><td>${key}</td><td>${data[key]}</td></tr>`);
+        }
+      }
+
+      table.innerHTML = tableContents.join('');
+      mainEl.appendChild(table);
+    })
+    .catch(err => {
+      mainEl.innerHTML = '';
+      p.textContent = err.message; //getData should return 'result.message'?
+      mainEl.appendChild(p);
+    })
+  
 }
 
 function getData() {
   /* 
     fetchDataを呼び出して、戻ってきたデータのsuccessの値を元にresolveで物件データまたは、rejectでエラーメッセージを返す。
   */
+  return fetchData() //return?
+    .then(result => {
+      if(result.success){
+        return Promise.resolve(result.propertyData);
+      } else {
+        return Promise.reject(result.message);
+      }
+    })
 }
 
 function fetchData() {
@@ -33,6 +63,17 @@ function fetchData() {
     lodashのrandom()を使って、80%の確率で正しいデータを返し、20%の確率でエラーを返すようにしましょう。
     またsetTimeoutを利用して、1秒待ってから結果を得るようにします。
   */
+  return new Promise((resolve, reject) => {
+    let rand = _.random(1, 10);
+    // console.log(rand);
+    setTimeout(() => {
+      if(rand >= 3 && rand <= 10){
+        resolve({ success: true, propertyData: propertyData });
+      } else {
+        reject({ success: false, message: 'データの取得に失敗しました。' });
+      }
+    }, 1000)
+  })
 }
 
 {
