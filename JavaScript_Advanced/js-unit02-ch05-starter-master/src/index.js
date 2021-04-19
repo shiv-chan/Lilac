0a6ff0913b2154f9fb9ef17036d3b17643b216a3
shiv-chan <kshibuya31@gmail.com>
2021-04-20 07:35:21 +0900

    Set localStorage functions.

--- JavaScript_Advanced/js-unit02-ch05-starter-master/src/index.js
+++ JavaScript_Advanced/js-unit02-ch05-starter-master/src/index.js
@@ -3,34 +3,35 @@ import Polyglot from 'node-polyglot';
 class TranslationApp {
 	constructor() {
 		this.polyglot = new Polyglot();
-		this.currentLocale = 'ja';
+		this.currentLocale = localStorage.getItem('locale') || 'ja';
 	}
 
-  /**
-   * Set a message to polyglot according to the current locale.
-   * Use polyglot.extend() to set a message.
-   */
+	/**
+	 * Set a message to polyglot according to the current locale.
+	 * Use polyglot.extend() to set a message.
+	 */
 	setup() {
 		this.polyglot.extend({
-			ja : 'こんにちは、世界',
-      en : 'Hello, World'
+			ja: 'こんにちは、世界',
+			en: 'Hello, World',
 		});
 
 		return this.polyglot.t(this.currentLocale);
 	}
 
-  /**
-   * Update the current locale according to data-local attribute in buttons.
-   */
+	/**
+	 * Update the current locale according to data-local attribute in buttons.
+	 */
 	updateLocale(e) {
 		this.currentLocale = e.target.dataset.locale;
-    app.showMessage();
+    localStorage.setItem('locale', this.currentLocale);
+		app.showMessage();
 	}
 
-  /**
-   * Show the message under the element with id named 'main' with h1 tag.
-   * This message should change by the current locale.
-   */
+	/**
+	 * Show the message under the element with id named 'main' with h1 tag.
+	 * This message should change by the current locale.
+	 */
 	showMessage() {
 		const main = document.querySelector('#main');
 		const h1 = document.createElement('h1');
