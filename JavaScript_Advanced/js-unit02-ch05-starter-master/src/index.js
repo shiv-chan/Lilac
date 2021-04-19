e3a069f0447c997fb8a09b1bb096326f55044da0
shiv-chan <kshibuya31@gmail.com>
2021-04-19 10:57:30 +0900

    Add files.

---
+++ JavaScript_Advanced/js-unit02-ch05-starter-master/src/index.js
@@ -0,0 +1,52 @@
+import Polyglot from 'node-polyglot';
+
+class TranslationApp {
+	constructor() {
+		this.polyglot = new Polyglot();
+		this.currentLocale = 'ja';
+	}
+
+  /**
+   * Set a message to polyglot according to the current locale.
+   * Use polyglot.extend() to set a message.
+   */
+	setup() {
+		this.polyglot.extend({
+			ja : 'こんにちは、世界',
+      en : 'Hello, World'
+		});
+
+		return this.polyglot.t(this.currentLocale);
+	}
+
+  /**
+   * Update the current locale according to data-local attribute in buttons.
+   */
+	updateLocale(e) {
+		this.currentLocale = e.target.dataset.locale;
+    // this.showMessage();
+	}
+
+  /**
+   * Show the message under the element with id named 'main' with h1 tag.
+   * This message should change by the current locale.
+   */
+	showMessage() {
+		const main = document.querySelector('#main');
+		const h1 = document.createElement('h1');
+		h1.innerHTML = this.setup();
+		main.append(h1);
+	}
+}
+
+/** instantiate Class TranslationApp */
+const app = new TranslationApp();
+app.showMessage();
+
+{
+	const button1 = document.getElementById('button1');
+	button1.addEventListener('click', app.updateLocale);
+
+	const button2 = document.getElementById('button2');
+	button2.addEventListener('click', app.updateLocale);
+}
